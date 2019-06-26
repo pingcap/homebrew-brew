2a00f0105d39597021264da127ae9abaf44917b2
Morgan Tocker <tocker@gmail.com>
2019-06-26 14:20:46 -0600

    small fix

--- Formula/tidb-server@2.1.rb
+++ Formula/tidb-server@2.1.rb
@@ -1,4 +1,4 @@
-class TidbServerAT20 < Formula
+class TidbServerAT21 < Formula
   desc "TiDB is a MySQL compatible distributed database"
   homepage "https://www.pingcap.com/en/"
   url "https://github.com/pingcap/tidb.git",
