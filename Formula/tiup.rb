83b7603a60d25611ddccf79fdb12130a59ac008c
srstack <srstack0203@gmail.com>
2021-12-23 16:31:51 +0800

    use make build

--- Formula/tiup.rb
+++ Formula/tiup.rb
@@ -1,27 +1,23 @@
 class Tiup < Formula
   desc "TiDB is a MySQL compatible distributed database, and tiup is a component manager for testing and using TiDB locally."
   homepage "https://www.pingcap.com"
-  url "https://tiup-mirrors.pingcap.com/install.sh"
-  sha256 "2784762fa6151539ecae3a975c21996036f4d7c82a4efe1f57dbefd1579c4b98"
+  url "https://github.com/pingcap/tiup.git",
+      tag:      "v1.8.1",
+      revision: "559b99d0acb7d07acddbb8cda0304b0f27c6ec1e"
   license "Apache-2.0"
-  version "1.8.1"
+  version "v1.8.1"
 
-  # depends_on "go" => :build
-  depends_on "curl" => :build
+  depends_on "go" => :build
+  # depends_on "curl" => :build
   depends_on "mysql-client" => :optional
 
   def install
-    # set tiup home
-    tiup_home = ENV["HOME"]+"/"+".tiup"
-
     # install
-    system "sh install.sh"
-
+    system "make", "tiup"
     # set tiup bin
-    bin_path = tiup_home+"/bin/tiup"
-    #root_path = tiup_home+"/bin/root.json"
+    bin_path = "bin/tiup"
     bin.install bin_path
-    #system "echo 'Installed path: ", tiup_home ,"'"
+ 
     # end
   end
 
@@ -57,15 +53,8 @@ class Tiup < Formula
 
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test tiup`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "tiup" "--version"
+    system "#{bin}/tiup" "--version"
   end
 end
