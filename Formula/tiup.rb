461ede50022bbe04389fef823e133df42b35445c
Qiμ$hiЯuí <39378935+srstack@users.noreply.github.com>
2022-07-28 15:25:22 +0800

    tiup v1.10.2

--- Formula/tiup.rb
+++ Formula/tiup.rb
@@ -2,9 +2,9 @@ class Tiup < Formula
   desc "TiDB is a MySQL compatible distributed database, and tiup is a component manager for testing and using TiDB locally."
   homepage "https://www.pingcap.com"
   url "https://github.com/pingcap/tiup.git",
-      tag:      "v1.10.1"
+      tag:      "v1.10.2"
   license "Apache-2.0"
-  version "v1.10.1"
+  version "v1.10.2"
 
   depends_on "go" => :build
   # depends_on "curl" => :build
